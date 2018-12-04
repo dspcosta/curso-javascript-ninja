adf1882114b38bdf73e23d4faee45fda86bc9f51
Daniel Costa <dcostabus@gmail.com>
2018-12-04 13:26:26 -0200

    Challenge-07

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -4,16 +4,22 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 */
 // ?
 
+var array = [5, "Daniel", undefined, 3, true];
+console.log(array);
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 // ?
+function recebeArray (array) {
+	return array;
+}
 
+console.log(recebeArray(array));
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
 // ?
-
+console.log(recebeArray(array)[1]);
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
@@ -22,17 +28,27 @@ segundo parâmetro.
 */
 // ?
 
+function arrayPosicao (array, posicao) {
+	var posicao = array[posicao];
+	return posicao;
+}
+
+console.log(arrayPosicao(array, 3));
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
 // ?
 
+var arrayTipos = [5, 'Daniel', true, undefined, NaN];
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
 // ?
-
+for (var i = 0; i < 5; i++) {
+	valorArray = arrayPosicao(arrayTipos, i);
+	console.log(valorArray);
+};
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
 livro. Dentro dessa função, declare uma variável que recebe um objeto com as
@@ -49,6 +65,43 @@ os livros.
 */
 // ?
 
+function book (nomeLivro = null) {
+
+	livros = {
+		'livro1': {
+		quantidadePaginas: 50,
+		autor: 'Roberto',
+		editora: 'Martin Fontes'}
+		, 
+		'livro2': {
+		quantidadePaginas: 200,
+		autor: 'Daniel',
+		editora: 'Saraiva'}
+		, 
+		'livro3': {
+		quantidadePaginas: 300,
+		autor: 'Carol',
+		editora: 'Darkside'}
+	}
+
+
+	console.log(nomeLivro);
+
+	if (nomeLivro == null) {
+	return livros;
+	}else{
+	return livros[nomeLivro];
+
+	}
+
+}
+
+console.log(book());
+console.log(book('livro1'));
+	
+	
+
+
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
@@ -60,6 +113,8 @@ usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
 // ?
+
+console.log('O livro livro1 tem ' + book('livro1').quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -3,17 +3,27 @@ Crie um array com 5 items (tipos variados).
 */
 // ?
 
+var myarray = [5, 'Daniel', 3, undefined, null];
+
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 // ?
 
+function addItem (item) {
+	myarray.push(item);
+
+	return myarray;
+}
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
 // ?
+
+addItem([true, 'Roberto', 3]);
+console.log(myarray);
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
@@ -22,29 +32,71 @@ frase:
 */
 // ?
 
+console.log('O segundo elemento do segundo array é ' + myarray[5][1]);
+
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
 // ?
 
+console.log('O primeiro array tem ' +myarray.length+' itens.');
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
 // ?
 
+console.log('O segundo array tem ' + myarray[5].length + ' itens.');
+
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
-console.log( 'Números pares entre 10 e 20:' );
+var x = 10;
+
+while (x >= 10 && x <= 20) {	
+	
+	var pares = x % 2;
+
+	
+
+	if (pares == 0) {
+		console.log( 'Números pares entre 10 e 20: ' + x );
+
+	}
+
+	
+	x++;
+
+
+}
+
+
 // ?
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
-console.log( 'Números ímpares entre 10 e 20:' );
+
+var x = 10;
+
+while (x >= 10 && x <= 20) {	
+	
+	var pares = x % 2;
+
+	
+
+	if (pares != 0) {
+		console.log( 'Números impares entre 10 e 20: ' + x );
+
+	}
+
+	
+	x++;
+
+
+}
 // ?
 
 /*
